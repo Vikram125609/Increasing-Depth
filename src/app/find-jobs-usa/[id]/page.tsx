199a9d6aee28e77c4f7e961da36d6cdb8d57a9f0
Vikram Singh <vs125609@gmail.com>
2024-02-25 14:29:14 +0530

    added git hooks

--- src/app/find-jobs-usa/[id]/page.tsx
+++ src/app/find-jobs-usa/[id]/page.tsx
@@ -1,18 +1,23 @@
-import axios, { AxiosResponse } from "axios";
+import React from "react";
 import Layout from "./layout";
-export default async function Page() {
+import axios, { AxiosResponse } from "axios";
+export default async function Page({ params }: { params: { id: string } }) {
   const data: AxiosResponse<{
     code: number;
     message: string;
     data: { adminJobs: any[] };
-  }> = await axios.post("https://c2cjobs.org/api/v1/jobseeker/list-all-jobs");
-  return <Layout>
-    {
-      data.data.data?.adminJobs.map((jobs) => {
-        return <>
-          <h1>{jobs.job_title}</h1>
-        </>
-      })
-    }
-  </Layout>;
-};
+  }> = await axios.post(`https://c2cjobs.org/api/v1/jobseeker/list-all-jobs`, {
+    id: params.id,
+  });
+  return (
+    <Layout>
+      {data.data.data?.adminJobs.map((jobs) => {
+        return (
+          <>
+            <h1>{jobs.job_title}</h1>
+          </>
+        );
+      })}
+    </Layout>
+  );
+}
