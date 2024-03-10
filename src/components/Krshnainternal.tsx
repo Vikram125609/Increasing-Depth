bf37c2a0149d897b4945086314ebf75b2580856e
Vikram Singh <vs125609@gmail.com>
2024-03-10 17:06:22 +0530

    changes

--- src/components/Krshnainternal.tsx
+++ src/components/Krshnainternal.tsx
@@ -5,7 +5,8 @@ export default function Krshnainternal() {
   const krshnaCount = useRecoilValue(krshnaCounter);
   return (
     <h1>
-      This is Krshnainteral and the count is via props and the count is{" "}
+      <h1>So the issue here was <span className="text-red-500">When the parent state is change and you pass the state via props the child state will gone a reset to original value so in that case you can use recoil or move your state to upper level</span></h1>
+      This is Krshnainteral and the count is via recoil and the count is{" "}
       {krshnaCount}
     </h1>
   );
--- src/components/Radhainternal.tsx
+++ src/components/Radhainternal.tsx
@@ -5,7 +5,15 @@ export default function Radhainternal() {
   const radhaCount = useRecoilValue(radhaCounter);
   return (
     <h1>
-      This is Radhainteral and the count is via props and the count is{" "}
+      <h1>
+        So the issue here was{" "}
+        <span className="text-red-500">
+          When the parent state is change and you pass the state via props the
+          child state will gone a reset to original value so in that case you
+          can use recoil or move your state to upper level
+        </span>
+      </h1>
+      This is Radhainteral and the count is via recoil and the count is{" "}
       {radhaCount}
     </h1>
   );
