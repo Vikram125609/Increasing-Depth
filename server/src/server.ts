fcb3af5bda7679189380af7a6244fc138afdcb19
Vikram Singh <vs125609@gmail.com>
2024-03-24 11:59:01 +0530

    changes in the server configuration files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from "express";
+
 const express = require("express");
 const route = require("./route");
 const cors = require("cors");
@@ -6,6 +8,10 @@ const port = 3000;
 app.use(express.json());
 app.use(cors({ origin: "https://increasing-depth.vercel.app/" }));
 app.use("/api/v1", route);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+module.exports = app;
