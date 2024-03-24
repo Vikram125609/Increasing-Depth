4e25aa19636376b79b23ae9c8c8c25fef3b5f02c
Vikram Singh <vs125609@gmail.com>
2024-03-24 10:44:05 +0530

    Changed the port

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ const express = require("express");
 const route = require("./route");
 const cors = require("cors");
 const app = express();
-const port = 8080;
+const port = 3000;
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use("/api/v1", route);
