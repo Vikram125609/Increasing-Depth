f7921add539c2275823ed421d08c8c4696c17bfd
Vikram Singh <vs125609@gmail.com>
2024-03-24 12:13:52 +0530

    Changes in the pagination

--- client/src/components/Pagination.tsx
+++ client/src/components/Pagination.tsx
@@ -1,23 +1,30 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
 
 interface Props {
   page: string;
 }
 
 export default function Pagination({ page }: Props) {
-  const router = useRouter();
+  const { replace } = useRouter();
+  const pathname = usePathname().split("/").slice(0, -1).join("/");
+  const searchParams = useSearchParams();
   return (
     <div className="m-12 flex justify-center gap-4">
       <button
+        disabled={+page === 1}
         className="cursor-pointer"
-        onClick={() => router.push(`/find-job-usa/${+page - 1}`)}
+        onClick={() => {
+          replace(`${pathname}/${+page - 1}?${searchParams.toString()}`);
+        }}
       >
         Prev
       </button>
       <button
         className="cursor-pointer"
-        onClick={() => router.push(`/find-job-usa/${+page + 1}`)}
+        onClick={() => {
+          replace(`${pathname}/${+page + 1}?${searchParams.toString()}`);
+        }}
       >
         Next
       </button>
